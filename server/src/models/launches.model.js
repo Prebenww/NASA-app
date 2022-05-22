536043b62fc648dfb774e072343389afb15b88ff
preben Wulff <prebenox@gmail.com>
2022-05-22 16:57:06 +0200

    Completed CRUD operations for the app

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -8,13 +8,17 @@ const launch = {
     mission: 'Kepler Exploration X',
     rocket: 'Explorer IS1',
     launchData: new Date('December 27, 2030'),
-    destination: 'Kepler-442 b',
+    target: 'Kepler-442 b',
     customers: ['ZTM', 'NASA'],
     upcoming: true,
     success: true,
 }
 
 launches.set(launch.flightNumber, launch);
+
+function existsLaunchWithId(launchId) {
+    return launches.has(launchId);
+}
 
 function getAllLaunches() {
     return Array.from(launches.values())
@@ -24,14 +28,26 @@ function addNewLaunch(launch) {
     latestFlightNumber++;
     launches.set(latestFlightNumber,
         Object.assign(launch, {
-        success: true,
-        upcoming: true,
-        customers: ['Zero to mastery', 'NASA'],
-        flightNumber: latestFlightNumber,
-    }));
+            success: true,
+            upcoming: true,
+            customers: ['Zero to mastery', 'NASA'],
+            flightNumber: latestFlightNumber,
+        }));
+}
+
+function abortLaunchById(launchId) {
+
+    // launches.delete(launchId) //This would work fine to just delete the whole launch.
+    const aborted = launches.get(launchId);
+    aborted.upcoming = false;
+    aborted.success = false;
+
+    return aborted;
 }
 
 module.exports = {
     getAllLaunches,
     addNewLaunch,
+    existsLaunchWithId,
+    abortLaunchById
 }
