08be52a92cf00f403d400215e5acd05aea4b6763
preben Wulff <prebenox@gmail.com>
2022-06-05 16:46:17 +0200

    added Docker container

--- server/src/server.js
+++ server/src/server.js
@@ -1,42 +1,24 @@
 const http = require('http');
-const mongoose = require('mongoose');
 
 require('dotenv').config();
 
 const app = require('./app');
-
+const { mongoConnect } = require('./services/mongo');
 const { loadPlanetsData } = require('./models/planets.model');
+const { loadLaunchData } = require('./models/launches.model');
 
 const PORT = process.env.PORT || 8000;
-
-const MONGO_URL = 'mongodb+srv://nasa-api:antidoteX123@cluster0.73lym.mongodb.net/nasa?retryWrites=true&w=majority'
 
 const server = http.createServer(app);
 
 async function startServer() {
-
-    mongoose.connection.once('open', () => {
-        console.log('MongoDB connection ready!')
-    })
-
-    mongoose.connection.on('error', (err) => {
-        console.error(err);
-    })
-
-    await mongoose.connect(MONGO_URL);
-
-    await loadPlanetsData();
-
-
-    server.listen(PORT, () => {
-        console.log(`Listening on port ${PORT}...`);
-    });
+  await mongoConnect();
+  await loadPlanetsData();
+  await loadLaunchData();
+  
+  server.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}...`);
+  });
 }
 
-startServer()
-
-
-
-
-
-
+startServer();
